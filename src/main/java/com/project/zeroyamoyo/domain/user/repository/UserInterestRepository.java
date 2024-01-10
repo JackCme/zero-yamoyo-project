7e898111742ddc6dbfa3851847bd7903290d28fd
JackCme <pitou_106@naver.com>
2024-01-10 17:31:54 +0900

    fix: @Repository 어노테이션 누락

--- src/main/java/com/project/zeroyamoyo/domain/user/repository/UserInterestRepository.java
+++ src/main/java/com/project/zeroyamoyo/domain/user/repository/UserInterestRepository.java
@@ -2,6 +2,8 @@ package com.project.zeroyamoyo.domain.user.repository;
 
 import com.project.zeroyamoyo.domain.user.entity.UserInterest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserInterestRepository extends JpaRepository<UserInterest, Long> {
 }
