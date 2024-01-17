eac766c1208b50af12d502d9b2302b46f46e90af
JackCme <pitou_106@naver.com>
2024-01-17 16:50:27 +0900

    fix: GlobalException 상속받는 RuntimeException 오버라이딩 메소드 추가

--- src/main/java/com/project/zeroyamoyo/global/exception/GlobalException.java
+++ src/main/java/com/project/zeroyamoyo/global/exception/GlobalException.java
@@ -1,10 +1,33 @@
 package com.project.zeroyamoyo.global.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public class GlobalException extends RuntimeException {
     private final ResultType resultType;
+
+    public GlobalException(ResultType resultType) {
+        super();
+        this.resultType = resultType;
+    }
+
+    public GlobalException(String message) {
+        super(message);
+        resultType = null;
+    }
+
+    public GlobalException(String message, Throwable cause) {
+        super(message, cause);
+        resultType = null;
+    }
+
+    public GlobalException(Throwable cause) {
+        super(cause);
+        resultType = null;
+    }
+
+    protected GlobalException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        resultType = null;
+    }
 }
